0ed36850f564a7ecc64ce2b3d50f2efb4e314fb4
Alejandro Correa Rodríguez <smxacorrea@gmail.com>
2025-04-02 17:35:27 +0200

    hecho resourcedetail arreglado spinner y página

--- src/pages/ResourceDetailPage.jsx
+++ src/pages/ResourceDetailPage.jsx
@@ -37,30 +37,40 @@ const getComments = async () => {
   }
 };
 
-if (!resource || comments.length === 0) {// cláusula de guardia
-    return <div className="spinner"></div>
+if (!resource || comments === null) {  
+  return <div className="spinner"></div>;  
 }
 
+
+const volverAtras = () => {
+  navigate("/ResourcePage");
+};
+
 return (
-
   <div>
-
     <h1>{resource.title}</h1>
     <p>{resource.content}</p>
     <p>Categoria: {resource.category}</p>
     <p>Autor: {resource.author}</p>
 
-    <h2>Comentarios:</h2>
-      {comments.map((comment) => (
+    <button style={{position: "absolute", top: "20px", left: "20px", fontSize: "18px", padding: "10px 20px", backgroundColor: "#f2a90d"}}
+      onClick={volverAtras}>Atrás
+    </button>
 
+    <h2>Comentarios:</h2>
+
+    {comments === null || comments.length === 0 ? (
+      <p>No hay comentarios aún.</p>
+    ) : (
+      comments.map((comment) => (
         <div key={comment._id}>
           <p>{comment.user.username}: {comment.text}</p>
         </div>
-
-      ))}
-      
+      ))
+    )}
   </div>
 );
+
 }
 
 
