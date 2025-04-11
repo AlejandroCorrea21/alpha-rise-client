eb373bc011d42fe843fde27eb793e37dedfb78ff
Alejandro Correa Rodríguez <smxacorrea@gmail.com>
2025-04-11 17:30:02 +0200

    puesto error 500 correctamente

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
       <h1>Bienvenido a AlphaRise</h1>
-      <Link to="/ResourcePage">
-      <button style={{ fontSize: '18px', padding: '10px 20px', marginTop: '20px', textAlign: 'center', marginLeft: '30px', marginRight: '30px', backgroundColor: '#7aae3d' }}>Ver Recursos</button>
-      </Link>
+
+      <div style={{ marginTop: '20px' }}>
+        <img src="https://res.cloudinary.com/dtobkctj9/image/upload/v1744380122/305764477_165131682835779_7444225890165805066_n_ggw2yj.png" alt="Logo AlphaRise" style={{ maxWidth: "300px", width: "80%", height: "auto", borderRadius: "12px" }}
+        />
+      </div>
+
+      <div style={{ marginTop: '30px' }}>
+        <Link to="/ResourcePage">
+          <button style={{ fontSize: '18px', padding: '10px 20px', backgroundColor: '#7aae3d', border: 'none', borderRadius: '8px', color: '#fff', cursor: 'pointer' }}>
+            Ver Recursos</button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/ResourcePage.jsx
+++ src/pages/ResourcePage.jsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import service from "../services/config.services";
+import Error500 from "../components/Error500";
 
 function ResourcePage() {
   const [resources, setResources] = useState([]);
   const navigate = useNavigate();
   const [filterResources, setFilterResources] = useState("frase");
-
-  // console.log("Filtrando", filterResources);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     getResources();
-  }, [filterResources]); // lo monto y obtiene los recursos.
+  }, [filterResources]);
 
   const volverAtras = () => {
-    navigate("/"); // Va atrás al pulsar con el botón (página / home)
-  }
+    navigate("/");
+  };
+
   const getResources = async () => {
-    
     try {
       let response;
       if (filterResources) {
@@ -29,43 +28,50 @@ function ResourcePage() {
       setResources(response.data);
     } catch (error) {
       console.log(error);
+      setHasError(true);
     }
   };
+
+  if (hasError) {
+    return <Error500 />;
+  }
 
   return (
     <div>
       <h1>Todos los Recursos</h1>
 
-        <h2>Filtrar por Categoría</h2>
+      <h2>Filtrar por Categoría</h2>
 
-          <button style={{ margin: "5px", padding: "10px", backgroundColor: "#299039", color: "white", }} onClick={() => setFilterResources("frase")} >
-          Frase</button>
+      <button style={{ margin: "5px", padding: "10px", backgroundColor: "#299039", color: "white" }} onClick={() => setFilterResources("frase")}>
+        Frase</button>
 
-          <button style={{ margin: "5px", padding: "10px", backgroundColor: "#25348a", color: "white", }} onClick={() => setFilterResources("texto bíblico")} >
-          Texto</button>
+      <button style={{ margin: "5px", padding: "10px", backgroundColor: "#25348a", color: "white" }} onClick={() => setFilterResources("texto bíblico")}>
+        Texto</button>
 
-          <button style={{ margin: "5px", padding: "10px", backgroundColor: "#731b2c", color: "white", }} onClick={() => setFilterResources("libro")} >
-          Libros</button>
+      <button style={{ margin: "5px", padding: "10px", backgroundColor: "#731b2c", color: "white" }} onClick={() => setFilterResources("libro")}>
+        Libros</button>
 
-          <button style={{ margin: "5px", padding: "10px", backgroundColor: "#cd33ff", color: "white" }} onClick={() => setFilterResources("otro")}>
-          Otro</button>
+      <button style={{ margin: "5px", padding: "10px", backgroundColor: "#cd33ff", color: "white" }} onClick={() => setFilterResources("otro")}>
+        Otro</button>
 
-          <button style={{ margin: "5px", padding: "10px", backgroundColor: "#ff5733", color: "white" }} onClick={() => setFilterResources("")}>
-          Mostrar Todos</button>
+      <button style={{ margin: "5px", padding: "10px", backgroundColor: "#ff5733", color: "white" }} onClick={() => setFilterResources("")}>
+        Mostrar Todos</button>
+
+      <div className="resource-container">
 
         {resources.map((resource) => (
-
-          <div key={resource._id}>
-
-            <h2>{resource.title}</h2>
-            <h3>{resource.category}</h3>
-            <Link to={`/resources/${resource._id}`}>
-            <button style={{ backgroundColor: "#0373b8", color: "white", padding: "5px 10px", border: "none", cursor: "pointer", }}
-            >Ver Recurso</button>
-            </Link>
+          <div className="resource-card" key={resource._id}>
+            <div>
+              <h2>{resource.title}</h2>
+              <h3>{resource.category}</h3>
+              <Link to={`/resources/${resource._id}`}>
+                <button className="btn-see">Ver Recurso</button>
+              </Link>
+            </div>
           </div>
         ))}
-        
+
+      </div>
     </div>
   );
 }
